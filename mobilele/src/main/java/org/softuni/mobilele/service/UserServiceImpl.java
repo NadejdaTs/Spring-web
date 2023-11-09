ee5cbb584d0c67b944b99cd6a999d41061e30a89
NadejdaTs <nadezhda.tsvetanova@gmail.com>
2023-11-09 16:37:24 +0200

    Workshop

---
+++ mobilele/src/main/java/org/softuni/mobilele/service/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package org.softuni.mobilele.service;
+
+import org.softuni.mobilele.model.dto.UserRegistrationDTO;
+import org.softuni.mobilele.model.entity.UserEntity;
+import org.softuni.mobilele.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void registerUser(UserRegistrationDTO userRegistrationDTO) {
+//        System.out.println(userRegistrationDTO);
+        userRepository.save(map(userRegistrationDTO));
+    }
+
+    private UserEntity map(UserRegistrationDTO userRegistrationDTO){
+        return new UserEntity()
+                .setActive(true)
+                .setFirstName(userRegistrationDTO.firstName())
+                .setLastName(userRegistrationDTO.lastName())
+                .setEmail(userRegistrationDTO.email())
+                .setPassword(passwordEncoder.encode(userRegistrationDTO.password()));
+    }
+}
